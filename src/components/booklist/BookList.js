3b783bac178d377533d4686567c3f3fc1be49942
Dannizzzzz <oliviazhang096@gmail.com>
2023-04-22 23:09:31 +0930

    add title in webpage

---
+++ src/components/booklist/BookList.js
@@ -0,0 +1,14 @@
+import './BookList.css';
+
+function BookList() {
+  return (
+    <>
+      <div className="top">
+        <h1>Amazon Best Sellers</h1>
+        <h5>Our most popular products based on sales. Updated frequently.</h5>
+      </div>
+    </>
+  );
+}
+
+export default BookList;
