658b1c4f3de471c98e988131c79e8f928bfcdb38
Dannizzzzz <oliviazhang096@gmail.com>
2023-04-30 13:39:04 +0930

    use props to pass books data to Book component

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,12 +1,13 @@
 import './Book.css';
 
 const Book = (props) => {
-  const { img, title, author, price } = props;
+  const { img, title, author, type, price } = props;
   return (
     <div className="book">
       <img src={img} alt={title} />
-      <p className="title">{title}</p>
+      <p className="name">{title}</p>
       <p className="author">{author}</p>
+      <p className="type">{type}</p>
       <p className="price">{price}</p>
     </div>
   );
