f3d45a4258e1f27f5a4dceabfe2f9ae1ad62a697
Dannizzzzz <oliviazhang096@gmail.com>
2023-04-30 18:18:10 +0930

    add rank number on top of the pics

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,9 +1,13 @@
 import './Book.css';
 
 const Book = (props) => {
-  const { img, title, author, type, price } = props;
+  const { img, title, author, type, price, id } = props;
   return (
     <div className="book">
+      <div className="rank">
+        <span className="rankNum">#{id}</span>
+      </div>
+      <div className="rankTri"></div>
       <img src={img} alt={title} />
       <p className="name">{title}</p>
       <p className="author">{author}</p>
